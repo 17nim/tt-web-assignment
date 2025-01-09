0c3f966eb77e9b7d56fcea9b96971d7d439ef8fc
17nim <17nim.m@gmail.com>
2025-01-09 20:54:58 +0700

    Establish connection to local MySQL server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,29 @@
+const express = require('express')
+const app = express()
 const mysql = require('mysql2/promise')
+// require('dotenv').config()
 
-app.get()
+const port = 8000
+
+app.get('/hello_world', (req, res) => {
+    res.send('Hello, world!')
+})
+
+app.listen(port, () => {
+    console.log(`The server is live! Port: ${port}`)
+})
+
+app.get('/usersdb', (req, res) => {
+    mysql.createConnection({
+        host: 'localhost',
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: 'tt_assignment_db'
+    }).then((conn) => {
+        conn
+            .query('SELECT * FROM users')
+            .then((results) => {
+                res.json(results[0])
+            })
+    })
+})
