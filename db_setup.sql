8d9a8cca0d980e74bf5c05bd5687b3f23e1256e5
17nim <17nim.m@gmail.com>
2025-01-16 10:25:07 +0700

    Clean up comments

--- db_setup.sql
+++ db_setup.sql
@@ -3,7 +3,6 @@ CREATE DATABASE IF NOT EXISTS tt_assignment_db;
 USE tt_assignment_db;
 
 CREATE TABLE IF NOT EXISTS users (
-    -- id INT AUTO_INCREMENT PRIMARY KEY,
     hn INT(6) AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(100),
     last_name VARCHAR(100),
